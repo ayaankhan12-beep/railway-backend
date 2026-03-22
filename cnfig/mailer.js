93c6284ec7f33331843c37012bc8a93f830b45fb
ayaankhan12-beep <muhammadayaanmuhammadshoaib@gmail.com>
2026-03-22 20:12:55 +0500

    st

--- cnfig/mailer.js
+++ cnfig/mailer.js
@@ -1,7 +1,8 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.sendinblue.com",
+  port:587,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.APP_PASS, // App password
